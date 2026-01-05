37fdcd0dd2df28718ba9d2edf301e77b235b1074
Gary Lo <gaplotech@gmail.com>
2026-01-05 10:45:46 +0800

    fix app.js version issue

--- lab03-contextual-hooks/app.js
+++ lab03-contextual-hooks/app.js
@@ -1,12 +1,6 @@
-// Greets a user by name
 const greet = (name) => `Hello, ${name}!`;
 
-// Adds two numbers together
 const add = (a, b) => a + b;
-
-// Subtracts the second number from the first
-const subtract = (a, b) => a - b;
 
 console.log(greet("Kiro"));
 console.log("2 + 3 =", add(2, 3));
-console.log("5 - 2 =", subtract(5, 2));
